c5b5ed846041c9252d327f0a84c548be735220ac
iamcyruss <russnicosia@gmail.com>
2023-11-28 17:18:45 -0700

    mostly-destinations

--- app.py
+++ app.py
@@ -18,5 +18,29 @@ def contact():
 def destinations():
         return render_template('destinations.html')
 
+@app.route('/indonesia')
+def indonesia():
+        return render_template('indonesia.html')
+
+@app.route('/maldives')
+def maldives():
+        return render_template('maldives.html')
+
+@app.route('/galapagos')
+def galapagos():
+        return render_template('galapagos.html')
+
+@app.route('/french-polynesia')
+def french_polynesia():
+        return render_template('french-polynesia.html')
+
+@app.route('/papua-new-guinea')
+def papua_new_guinea():
+        return render_template('papua-new-guinea.html')
+
+@app.route('/fiji')
+def fiji():
+        return render_template('fiji.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
